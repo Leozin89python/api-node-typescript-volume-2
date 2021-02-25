6de479eeefb9e27d201b2e189aac795aecc4a24c
Leozin89Python <leozin069@gmail.com>
2021-02-25 20:28:23 -0300

    api-ts-node-express-version.1.1.8

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -6,7 +6,15 @@ const itemsRouter = Router()
 itemsRouter.get('/', async(req,res)=>{
     const items = await Knex('items').select('*')
 
-    return res.json(items)
+    const serializedItems = items.map(item =>{
+        return {
+            id:item.id,
+            title:item.title,
+            image_url:`http://localhost:3004/uploads/${item.image}`
+        }
+    })
+
+    return res.json(serializedItems)
 })
 
 export default itemsRouter
