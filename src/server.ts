1c77bc1d979e126f0f487ebcdc465b54189a3dd3
Leozin89Python <leozin069@gmail.com>
2021-02-25 17:18:16 -0300

    api-ts-node-express-version.1.1.5

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 const port :number = 3004
 
 import routes from './routes/route'
-
+import path from 'path'
 import express from 'express'
 const app = express()
 
 app.use(routes)
+
+app.use('/uploads',express.static(path.resolve(__dirname, '..', 'uploads')))
+
 
 app.listen(port)
